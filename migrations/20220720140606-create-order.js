eb1a0223dd8437abad204498f66b8e96af12b36c
faturachmanyusup <fatuachmany@gmail.com>
2022-07-20 21:08:29 +0700

    Add new model Order

---
+++ migrations/20220720140606-create-order.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      customer_id: {
+        type: Sequelize.UUID
+      },
+      item_id: {
+        type: Sequelize.UUID
+      },
+      qty: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Orders');
+  }
+};

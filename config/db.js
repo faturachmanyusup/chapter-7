cb6c3396b51a5efdf0209bcb7c1bb4a3a4be6f0f
faturachmanyusup <fatuachmany@gmail.com>
2022-07-29 19:58:03 +0700

    Configure production environment

--- config/db.js
+++ config/db.js
@@ -20,10 +20,12 @@ module.exports = {
     "logging": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER_DEV,
+    "password": process.env.DB_PASS_DEV,
+    "database": process.env.DB_NAME_DEV,
+    "host": process.env.DB_HOST_DEV,
+    "port": process.env.DB_POR_DEV,
+    "dialect": process.env.DB_DIALECT_DEV,
+    "logging": false
   }
 }
